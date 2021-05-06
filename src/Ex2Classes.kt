5c5c5214dc54258a8a004a7e774d0deaa219d848
João Paulo Faustino Santos <jpaulofsantos@gmail.com>
2021-05-06 16:06:27 -0300

    add new method using reduce()

--- src/Ex2Classes.kt
+++ src/Ex2Classes.kt
@@ -1,6 +1,5 @@
 class Ex2Classes(var lista: List<Int>) {
 
-
     fun multiplicaNumerosLista(): Int {
         var list = this.lista
         var result: Int = 1
@@ -9,9 +8,17 @@ class Ex2Classes(var lista: List<Int>) {
         }
         return result
     }
+
+    fun multiplica(): Int {
+        var list = this.lista
+        var num = list.reduce {a, b -> a * b}
+        return num
+    }
+
 }
 
 fun main() {
     var ex2 = Ex2Classes(listOf(2,3,2,5))
     println(ex2.multiplicaNumerosLista())
+    println(ex2.multiplica())
 }
